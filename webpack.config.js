7cbe7e0596456c4b500d2f9ebac1b446934cf356
TropicalRaisel <tshuler@nationalraisin.com>
2021-08-21 15:41:12 -0700

    Added Progressive Web App support

--- webpack.config.js
+++ webpack.config.js
@@ -76,4 +76,14 @@ Encore
     //.autoProvidejQuery()
     ;
 
+if (Encore.isProduction()) {
+    Encore
+        .addPlugin(new WorkboxWebpackPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true
+        }))
+}
+
 module.exports = Encore.getWebpackConfig();
