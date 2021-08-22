a0f20efe0796fc5f8bec32051492332a481b3afc
TropicalRaisel <tshuler@nationalraisin.com>
2021-08-22 00:07:05 -0700

    Configured ESLint

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -2,17 +2,17 @@ import '../styles/app.scss'
 import 'jquery'
 import 'bootstrap' // load bootstrap after jquery to register it as a plugin
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("service-worker.js")
-            .then((registration) => {
-                console.log("Service Worker registered: ", registration);
-            })
-            .catch((registrationError) => {
-                console.error("Service Worker registration failed: ", registrationError);
-            });
-    });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered: ', registration)
+      })
+      .catch((registrationError) => {
+        console.error('Service Worker registration failed: ', registrationError)
+      })
+  })
 }
 
 console.log('Hello World!')
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const Encore = require('@symfony/webpack-encore');
-const StylelintPlugin = require('stylelint-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const Encore = require('@symfony/webpack-encore')
+const StylelintPlugin = require('stylelint-webpack-plugin')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
 Encore
@@ -14,7 +14,7 @@ Encore
   // public path used by the web server to access the output path
   .setPublicPath('/build')
   // only needed for CDN's or sub-directory deploy
-  //.setManifestKeyPrefix('build/')
+  // .setManifestKeyPrefix('build/')
 
   /*
    * ENTRY CONFIG
@@ -26,8 +26,8 @@ Encore
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
   .addEntry('app', './assets/javascripts/app.js')
-  //.addEntry('page1', './assets/javascripts/page1.js')
-  //.addEntry('page2', './assets/javascripts/page2.js')
+  // .addEntry('page1', './assets/javascripts/page1.js')
+  // .addEntry('page2', './assets/javascripts/page2.js')
 
   // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .splitEntryChunks()
@@ -44,17 +44,17 @@ Encore
    * https://symfony.com/doc/current/frontend.html#adding-more-features
    */
   .cleanupOutputBeforeBuild()
-  //.enableBuildNotifications()
+  // .enableBuildNotifications()
   .enableBuildCache({ config: [__filename] }, (cache) => {
-    cache.version = `${process.env.GIT_REV}`;
-    cache.name = `${process.env.target}`;
+    cache.version = `${process.env.GIT_REV}`
+    cache.name = `${process.env.target}`
   })
   .enableSourceMaps(!Encore.isProduction())
   .configureDevServerOptions((config) => {
     // https://github.com/symfony/webpack-encore/issues/1017#issuecomment-887264214
     delete config.client.host
 
-    config.allowedHosts = 'all';
+    config.allowedHosts = 'all'
     // in older Webpack Dev Server versions, use this option instead:
     // config.firewall = false;
   })
@@ -64,8 +64,8 @@ Encore
 
   // enables @babel/preset-env polyfills
   .configureBabelPresetEnv((config) => {
-    config.useBuiltIns = 'usage';
-    config.corejs = 3;
+    config.useBuiltIns = 'usage'
+    config.corejs = 3
   })
 
   // enables Sass/SCSS support
@@ -80,7 +80,6 @@ Encore
 
   // uncomment if you're having problems with a jQuery plugin
   .autoProvidejQuery()
-  ;
 
 if (Encore.isDev()) {
   Encore
@@ -98,4 +97,4 @@ if (Encore.isDev()) {
     }))
 }
 
-module.exports = Encore.getWebpackConfig();
+module.exports = Encore.getWebpackConfig()
