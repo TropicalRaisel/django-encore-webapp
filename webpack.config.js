eea826290f3599c94eb83cc454d0752fe0f20ff2
TropicalRaisel <tshuler@nationalraisin.com>
2021-08-23 08:42:28 -0700

    Fixed dev server runtime

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,9 @@ Encore
   // directory where compiled assets will be stored
   .setOutputPath('public/build/')
   // public path used by the web server to access the output path
-  .setPublicPath('/static/build')
+  .setPublicPath('/build/')
   // only needed for CDN's or sub-directory deploy
-  .setManifestKeyPrefix('static/build/')
+  .setManifestKeyPrefix('build/')
 
   /*
    * ENTRY CONFIG
