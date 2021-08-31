ffe8c8035551d547b8d66c5393ba57cef1667c91
TropicalRaisel <tshuler@nationalraisin.com>
2021-08-31 11:38:21 -0700

    Moved DEBUG to an env variable

--- core/settings.py
+++ core/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env["SECRET_KEY"]
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(env["DEBUG"])
 
 ALLOWED_HOSTS = []
 
@@ -55,7 +53,7 @@ MIDDLEWARE = [
 
 # htmlmin settings
 
-HTML_MINIFY = True
+HTML_MINIFY = not DEBUG
 
 KEEP_COMMENTS_ON_MINIFYING = False
 
