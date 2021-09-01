e3ce22c1049872b282a23b07dbc0dd554e14586e
TropicalRaisel <tshuler@nationalraisin.com>
2021-08-31 21:34:05 -0700

    Created a zero install Yarn 2 baseline

--- webpack.config.js
+++ webpack.config.js
@@ -1,204 +1,96 @@
-const Encore = require('@symfony/webpack-encore')
-const svgToMiniDataURI = require('mini-svg-data-uri')
-const WebpackBar = require('webpackbar')
-const DashboardPlugin = require('webpack-dashboard/plugin')
-const OpenBrowserPlugin = require('webpack-open-browser-plugin')
-const ESLintWebpackPlugin = require('eslint-webpack-plugin')
-const StylelintWebpackPlugin = require('stylelint-webpack-plugin')
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
-const CircularDependencyPlugin = require('circular-dependency-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const { GenerateSW } = require('workbox-webpack-plugin')
-const WebpackObfuscator = require('webpack-obfuscator')
+const Encore = require('@symfony/webpack-encore');
+const Path = require('path')
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
-  // directory where compiled assets will be stored
-  .setOutputPath('public/build/')
-  // public path used by the web server to access the output path
-  .setPublicPath('/build/')
-  // only needed for CDN's or sub-directory deploy
-  .setManifestKeyPrefix('build/')
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    .setManifestKeyPrefix('build/')
 
-  /*
-   * ENTRY CONFIG
-   *
-   * Add 1 entry for each "page" of your app
-   * (including one that's included on every page - e.g. "app")
-   *
-   * Each entry will result in one JavaScript file (e.g. app.js)
-   * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-   */
-  .addEntry('app', './assets/javascripts/app.js')
-  .addEntry('home', './assets/javascripts/home.js')
-  // .addEntry('page2', './assets/javascripts/page2.js')
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './assets/javascripts/app.js')
+    .addEntry('home', './assets/javascripts/home.js')
+    //.addEntry('page2', './assets/page2.js')
 
-  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-  .splitEntryChunks()
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
 
-  // will require an extra script tag for runtime.js
-  // but, you probably want this, unless you're building a single-page app
-  .enableSingleRuntimeChunk()
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
 
-  /*
-   * FEATURE CONFIG
-   *
-   * Enable & configure other features below. For a full
-   * list of features, see:
-   * https://symfony.com/doc/current/frontend.html#adding-more-features
-   */
-  .cleanupOutputBeforeBuild()
-  .enableSourceMaps(!Encore.isProduction())
-  .configureDevServerOptions((config) => {
-    // fixes cors header issues
-    config.headers = {
-      'Access-Control-Allow-Origin': '*'
-    }
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications(true, (options) => {
+      options.skipFirstNotification = true
+    })
+    .enableSourceMaps(!Encore.isProduction())
+    .configureDevServerOptions((options) => {
+      // https://github.com/symfony/webpack-encore/issues/1017#issuecomment-887264214
+      delete options.client.host
 
-    // https://github.com/symfony/webpack-encore/issues/1017#issuecomment-887264214
-    delete config.client.host
-  })
+      // fixes cors header issues
+      options.headers = {
+        'Access-Control-Allow-Origin': '*'
+      }
 
-  // enables hashed filenames (e.g. app.abc123.css)
-  .enableVersioning(Encore.isProduction())
-
-  .addPlugin(new ImageMinimizerPlugin({
-    minimizerOptions: {
-      // Lossless optimization with custom option
-      // Feel free to experiment with options for better result for you
-      plugins: [
-        ['gifsicle', { interlaced: true }],
-        ['jpegtran', { progressive: true }],
-        ['optipng', { optimizationLevel: 5 }],
-        // https://github.com/nuxt-community/imagemin-module/issues/28#issuecomment-886855337
-        ['svgo', { plugins: [{ name: 'removeViewBox', active: false }] }]
-      ]
-    }
-  }))
-
-  .configureImageRule({
-    type: 'asset'
-  }, () => {
-    return {
-      test: /\.svg$/i,
-      type: 'asset/inline',
-      generator: {
-        dataUrl: content => {
-          content = content.toString()
-          return svgToMiniDataURI(content)
+      options.static = {
+        directory: Path.join(__dirname, 'assets'),
+        staticOptions: {
+          index: 'views/index.html'
         }
       }
-    }
-  })
 
-  .configureFontRule({
-    type: 'asset'
-  })
+      options.open = {
+        target: ['/'],
+        app: {
+          name: 'chrome'
+        }
+      }
+    })
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
 
-  .copyFiles({
-    from: './assets/images',
-    to: 'images/[path][name].[hash:8].[ext]'
-  })
-
-  // enables @babel/preset-env polyfills
-  .configureBabelPresetEnv((config) => {
-    config.useBuiltIns = 'usage'
-    config.corejs = 3
-  })
-
-  // enables Sass/SCSS support
-  .enableSassLoader()
-
-  // enables CSS & polyfill support alongside Sass
-  .enablePostCssLoader()
-
-  // uncomment to get integrity="..." attributes on your script & link tags
-  // requires WebpackEncoreBundle 1.4 or higher
-  .enableIntegrityHashes(Encore.isProduction())
-
-  // uncomment if you're having problems with a jQuery plugin
-  .autoProvidejQuery()
-
-if (Encore.isDevServer()) {
-  Encore
-
-    .addPlugin(new DashboardPlugin())
-
-    .addPlugin(new OpenBrowserPlugin({
-      url: 'http://127.0.0.1:8000/'
-    }))
-
-    .enableBuildCache({ config: [__filename] }, (cache) => {
-      cache.version = `${process.env.GIT_REV}`
-      cache.name = `${process.env.target}`
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
     })
 
-    .enableBuildNotifications(true, (config) => {
-      config.skipFirstNotification = true
-    })
-} else {
-  Encore
+    // enables Sass/SCSS support
+    .enableSassLoader()
 
-    .addPlugin(new BundleAnalyzerPlugin({
-      analyzerMode: Encore.isDev() ? 'static' : 'disabled',
-      reportFilename: 'webpack-report.html',
-      openAnalyzer: Encore.isDev(),
-      logLevel: 'warn'
-    }))
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
 
-    .addPlugin(new WebpackBar({
-      color: 'lightblue'
-    }))
-}
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    .enableIntegrityHashes(Encore.isProduction())
 
-if (Encore.isDev()) {
-  const linterConfig = {
-    fix: true,
-    cache: true,
-    threads: true
-  }
+    // uncomment if you're having problems with a jQuery plugin
+    .autoProvidejQuery()
+;
 
-  Encore
-
-    .addPlugin(new ESLintWebpackPlugin(linterConfig))
-
-    .addPlugin(new StylelintWebpackPlugin(linterConfig))
-
-    .addPlugin(new CircularDependencyPlugin({
-      // exclude detection of files based on a RegExp
-      exclude: /node_modules/,
-      // include specific files based on a RegExp
-      // include: /javascripts/,
-      // add errors to webpack instead of warnings
-      failOnError: true
-      // allow import cycles that include an asyncronous import,
-      // e.g. via import(/* webpackMode: "weak" */ './file.js')
-      // allowAsyncCycles: false,
-      // set the current working directory for displaying module paths
-      // cwd: process.cwd()
-    }))
-} else {
-  Encore
-
-    .configureFriendlyErrorsPlugin((config) => {
-      config.logLevel = 'ERROR'
-    })
-
-    .addPlugin(new GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true
-    }))
-
-    .addPlugin(new WebpackObfuscator({
-      rotateStringArray: true
-    }))
-}
-
-module.exports = Encore.getWebpackConfig()
+module.exports = Encore.getWebpackConfig();
